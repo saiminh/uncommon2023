d8994bf3e741d1ef4b552e4f4f1691cc44cfd5a4
saiminh <saiminh@gmail.com>
2023-05-22 20:10:52 +0200

    image sizes

--- assets/js/dev/index.js
+++ assets/js/dev/index.js
@@ -5,9 +5,40 @@ global.motion = {
   scroll
 }
 
-window.addEventListener("load", () => {
+function detectDarkBg(){
   const contentWrapper = document.querySelector("main > .entry-content");
   if ( contentWrapper.querySelector(':first-child').classList.contains('has-contrast-background-color')) {
     document.body.classList.add('uncommon-content-has-background');
   }
+}
+
+function videoAutoPlayOnlyWhenIntersecting(){
+  // check for video elements on the page
+  const videos = document.querySelectorAll('video');
+  if (videos.length > 0) {
+    // create an IntersectionObserver instance
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // play the video
+          entry.target.play();
+        } else {
+          // pause the video
+          entry.target.pause();
+        }
+      });
+    })
+    // start observing
+    videos.forEach((video) => {
+      if (video.autoplay) {
+        observer.observe(video);
+      }
+    })
+  }
+}
+
+
+window.addEventListener("load", () => {
+  detectDarkBg();
+  videoAutoPlayOnlyWhenIntersecting();
 })
--- inc/image-sizes.php
+++ inc/image-sizes.php
@@ -8,12 +8,25 @@ function uncommon_add_image_sizes() {
   add_image_size( 'square-xl', 1080, 1080, true );
   add_image_size( 'square-xxl', 1280, 1280, true );
   add_image_size( 'square-xxl', 1620, 1620, true );
+  add_image_size( 'rectangle-portrait-xs', 240, 320 );
+  add_image_size( 'rectangle-portrait-s', 320 , 430 );
+  add_image_size( 'rectangle-portrait', 480, 640 );
+  add_image_size( 'rectangle-portrait-l', 720, 960 );
+  add_image_size( 'rectangle-portrait-xl', 960, 1280 );
+  add_image_size( 'rectangle-portrait-xxl', 1440, 1920 );
+  add_image_size( 'rectangle-landscape-xs', 320, 240 );
+  add_image_size( 'rectangle-landscape-s', 430, 320 );
+  add_image_size( 'rectangle-landscape', 640, 480 );
+  add_image_size( 'rectangle-landscape-l', 960, 720 );
+  add_image_size( 'rectangle-landscape-xl', 1280, 960 );
+  add_image_size( 'rectangle-landscape-xxl', 1920, 1440 );
 }
 add_action('after_setup_theme', 'uncommon_add_image_sizes');
 
 function uncommon_custom_sizes( $sizes ) {
   return array_merge( $sizes, array(
     'square' => __( 'Square for Homepage Header' ),
+    'rectangle-portrait' => __( 'Rectangle Portrait' ),
   ) );
 }
 
@@ -25,7 +38,10 @@ function my_content_image_sizes_attr( $sizes, $size, $image_src, $image_meta, $a
   
   //square for homepage header
   if ( $width == 640 && $height == 640 ) {
-    $sizes = '(min-width: 600px) 40vw, 81vw';
+    $sizes = '(min-width: 600px) 33vw, 75vw';
+  }  
+  if ( $width == 480 && $height == 640 ) {
+    $sizes = '(min-width: 600px) 50vw, 90vw';
   }  
   return $sizes;
 }
